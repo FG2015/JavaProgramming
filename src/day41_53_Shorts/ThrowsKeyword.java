9108e331ae8d05a86434185fa06b6b58bbc32a71
Farid <ferid2805@gmail.com>
2023-02-03 19:08:31 +0200

    ThrowsKeyword

---
+++ src/day41_53_Shorts/ThrowsKeyword.java
@@ -0,0 +1,12 @@
+package day41_53_Shorts;
+
+public class ThrowsKeyword {
+    public static void main(String[] args) throws InterruptedException{
+
+        System.out.println("hi");
+        Thread.sleep(3000);
+        System.out.println("salam");
+        Thread.sleep(4000);
+        System.out.println("gamarjobat");
+    }
+}
