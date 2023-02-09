cc32c8f70bb81cd6403bc0b7a2e80c5d306201be
Farid <ferid2805@gmail.com>
2023-02-09 17:37:59 +0200

    Polymorphism_Intro

---
+++ src/day41_53_Shorts/Polymorphism_Intro.java
@@ -0,0 +1,8 @@
+package day41_53_Shorts;
+
+public class Polymorphism_Intro {
+    public static void main(String[] args) {
+
+        Shape shape1=new Circle();
+        System.out.println(shape1.area());    }
+}
